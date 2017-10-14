5852fed3f421576818bc30e0c2a8a2c209488415
Borey-PC <buttscl@mail.uc.edu>
2017-10-14 16:15:33 -0400

    Added enemy explosions. Fixed small boss bullets. Fixed player speed

---
+++ Assets/AddBullets_SmallBoss_Ring.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddBullets_SmallBoss_Ring : MonoBehaviour
+{
+    int spawn_probabilty = 150; // Sets the probability that bullets will spawn due to music intensity
+    float intensity_count = 0; // Increases based on music's bass intensity
+    int count = 0;
+    public GameObject bullet_ring_prefab;
+
+    void Start ()
+    {
+
+	}
+	
+	void Update ()
+    {
+        intensity_count += BuildGround.average_low * 100;
+
+        if (intensity_count > spawn_probabilty)
+        {
+            Instantiate(bullet_ring_prefab, MoveEnemy_SmallBoss.position, transform.localRotation);
+            intensity_count = 0;
+        }
+    }
+}
---
+++ Assets/DestroyObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour
+{
+    int count = 0;
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        count++;
+        if (count > 120)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
